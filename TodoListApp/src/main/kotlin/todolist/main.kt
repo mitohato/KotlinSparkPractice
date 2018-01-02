523ff923a66f6542e5a13ef671bdfdc59bb19295
mitohato <mitohato14@gmail.com>
2018-01-03 02:52:44 +0900

    Fix main.kt

--- TodoListApp/src/main/kotlin/todolist/main.kt
+++ TodoListApp/src/main/kotlin/todolist/main.kt
@@ -4,16 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import spark.Spark.get
 
-class main {
-    fun main(args: Array<String>) {
-        val objectMapper = ObjectMapper().registerKotlinModule()
-        get("/tasks") { _, _ ->
-            val tasks = listOf(
-                    Task(1, "cleaning", false),
-                    Task(2, "Post", true)
-            )
-            objectMapper.writeValueAsString(tasks)
+fun main(args: Array<String>) {
+    val objectMapper = ObjectMapper().registerKotlinModule()
+    get("/tasks") { _, _ ->
+        val tasks = listOf(
+                Task(1, "cleaning", false),
+                Task(2, "Post", true)
+        )
+        objectMapper.writeValueAsString(tasks)
 
-        }
     }
 }
