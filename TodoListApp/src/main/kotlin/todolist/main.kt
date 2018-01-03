01bcd2ac8034d0ef04129b2676adc1c53ee3e62d
mitohato <mitohato14@gmail.com>
2018-01-04 00:08:34 +0900

    Clean up path

--- TodoListApp/src/main/kotlin/todolist/main.kt
+++ TodoListApp/src/main/kotlin/todolist/main.kt
@@ -2,8 +2,7 @@ package todolist
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import spark.Spark.get
-import spark.Spark.post
+import spark.Spark.*
 
 fun main(args: Array<String>) {
     val objectMapper = ObjectMapper().registerKotlinModule()
@@ -11,7 +10,10 @@ fun main(args: Array<String>) {
     val taskRepository = TaskRepository()
     val taskController = TaskController(taskRepository, objectMapper)
 
-    get("/tasks", taskController.index(), jsonTransformer)
-    post("/tasks", taskController.create(), jsonTransformer)
-    get("/tasks/:id", taskController.show(), jsonTransformer)
+    path("/tasks") {
+        get("", taskController.index(), jsonTransformer)
+        post("", taskController.create(), jsonTransformer)
+        get("/:id", taskController.show(), jsonTransformer)
+        delete("/:id", taskController.destroy(), jsonTransformer)
+    }
 }
