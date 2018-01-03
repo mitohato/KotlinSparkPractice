249bd13cae204d3b29d01f7ccf20314745451ad6
mitohato <mitohato14@gmail.com>
2018-01-03 23:49:33 +0900

    Adddestroy function

--- TodoListApp/src/main/kotlin/todolist/TaskController.kt
+++ TodoListApp/src/main/kotlin/todolist/TaskController.kt
@@ -1,10 +1,17 @@
 package todolist
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import spark.Request
 import spark.Route
 import spark.Spark.halt
 
 class TaskController(private val taskRepository: TaskRepository, private val objectMapper: ObjectMapper) {
+    private val Request.task: Task?
+        get() {
+            val id = params("id").toLongOrNull()
+            return id?.let(taskRepository::findById)
+        }
+
     fun index(): Route = Route { _, _ ->
         taskRepository.findAll()
     }
@@ -17,7 +24,12 @@ class TaskController(private val taskRepository: TaskRepository, private val obj
     }
 
     fun show(): Route = Route { req, _ ->
-        val id = req.params("id").toLongOrNull()
-        id?.let(taskRepository::findById) ?: throw halt(404)
+        req.task ?: throw halt(404)
+    }
+
+    fun destroy(): Route = Route { req, res ->
+        val task = req.task ?: throw halt(404)
+        taskRepository.delete(task)
+        res.status(204)
     }
 }
